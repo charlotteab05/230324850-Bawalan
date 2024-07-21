7ec6990e1cd9049bec74236e4578b19e9510f422
root <root@LAPTOP-6HM2IIJQ>
2024-07-22 00:55:45 +0800

    Add demofiles-fnl directory and its contents

---
+++ demofiles-fnl/samplecode.py
@@ -0,0 +1,13 @@
+# Sample Python script: samplecode.py
+
+# Print a greeting message
+print("Hello, World!")
+
+# Perform a simple arithmetic operation
+a = 5
+b = 3
+sum = a + b
+
+# Print the result
+print(f"The sum of {a} and {b} is {sum}")
+
